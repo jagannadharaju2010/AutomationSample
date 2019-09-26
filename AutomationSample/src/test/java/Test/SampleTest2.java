c4c1478bd3a1c06413fd0be7156db88a0c8eb9d6
HOME <HOME@192.168.0.4>
2019-09-26 12:46:20 +0530

    Commit the code

---
+++ AutomationSample/src/test/java/Test/SampleTest2.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest2 {
+@Test
+public void math2()
+{
+	System.out.println("Sample execution two");
+	Reporter.log("Sample execution two",true);
+}
+}
